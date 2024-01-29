9163cbc9966819c4ad83ce8c29ea966cc2758b95
Andrew Reynolds <areynold@illinois.edu>
2024-01-29 13:37:20 -0600

    update addon constants

---
+++ constants.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace IllinoisPublicMedia\NprStoryApi;
+
+if (!defined('BASEPATH')) {
+    exit('No direct script access allowed.');
+}
+
+class Constants
+{
+    const NAME = 'NPR Content Distribution System (CDS)';
+
+    const AUTHOR = 'Illinois Public Media';
+
+    const AUTHOR_URL = 'https://will.illinois.edu';
+
+    const DESCRIPTION = "An ExpressionEngine port of Open Public Media's NPR CDS wordpress module (https://github.com/OpenPublicMedia/npr-cds-wordpress).";
+
+    const DOCS_URL = 'https://github.com/willpublicmedia/npr-cds-expressionengine';
+
+    const NAMESPACE  = 'IllinoisPublicMedia\NprCds';
+
+    const VERSION = '0.0.0';
+}
