69c4e0ccafa816c92f0be75d2d3e75700757bcbe
Andrew Reynolds <areynold@illinois.edu>
2024-01-30 11:22:24 -0600

    enable module

---
+++ mod.npr_cds.php
@@ -0,0 +1,12 @@
+<?php if (!defined('BASEPATH')) {
+    exit('No direct script access allowed');
+}
+
+class Npr_cds
+{
+    public $return_data;
+
+    public function __construct()
+    {
+    }
+}
