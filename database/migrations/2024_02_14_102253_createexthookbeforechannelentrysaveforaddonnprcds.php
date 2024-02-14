ce647164d699a72437a158939411ffedeb627df3
Andrew Reynolds <areynold@illinois.edu>
2024-02-14 10:40:29 -0600

    install all extensions

--- database/migrations/2024_02_14_102253_createexthookbeforechannelentrysaveforaddonnprcds.php
+++ database/migrations/2024_02_14_102253_createexthookbeforechannelentrysaveforaddonnprcds.php
@@ -4,6 +4,13 @@ use ExpressionEngine\Service\Migration\Migration;
 
 class CreateExtHookBeforeChannelEntrySaveForAddonNprCds extends Migration
 {
+    private $required_extensions = array(
+        'query_cds' => array(
+            'hook' => 'before_channel_entry_save',
+            'priority' => 10,
+        ),
+    );
+
     /**
      * Execute the migration
      * @return void
@@ -12,18 +19,19 @@ class CreateExtHookBeforeChannelEntrySaveForAddonNprCds extends Migration
     {
         $addon = ee('Addon')->get('npr_cds');
 
-        $ext = [
-            'class' => $addon->getExtensionClass(),
-            'method' => 'before_channel_entry_save',
-            'hook' => 'before_channel_entry_save',
-            'settings' => serialize([]),
-            'priority' => 10,
-            'version' => $addon->getVersion(),
-            'enabled' => 'y'
-        ];
+        foreach ($this->required_extensions as $method => $config) {
+            $data = [
+                'class' => $addon->getExtensionClass(),
+                'method' => $method,
+                'hook' => $config['hook'],
+                'settings' => serialize([]),
+                'priority' => $config['priority'],
+                'version' => $addon->getVersion(),
+                'enabled' => 'y'
+            ];
 
-        // If we didnt find a matching Extension, lets just insert it
-        ee('Model')->make('Extension', $ext)->save();
+            ee('Model')->make('Extension', $data)->save();
+        }
     }
 
     /**
--- ext.npr_cds.php
+++ ext.npr_cds.php
@@ -1,12 +1,19 @@
 <?php
 
-if (! defined('BASEPATH')) {
+if (!defined('BASEPATH')) {
     exit('No direct script access allowed');
 }
 
 use ExpressionEngine\Service\Addon\Extension;
+use IllinoisPublicMedia\NprCds\Extensions\BeforeChannelEntrySave;
 
 class Npr_cds_ext extends Extension
 {
     protected $addon_name = 'npr_cds';
+
+    public function query_cds($entry, $values)
+    {
+        $runner = new BeforeChannelEntrySave();
+        $runner->query_cds($entry, $values);
+    }
 }
