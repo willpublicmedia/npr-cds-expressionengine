0212b1ff956b2b30f8a82174871fa3b7f21e125b
Andrew Reynolds <areynold@illinois.edu>
2024-02-14 10:24:54 -0600

    stub initial extension

---
+++ Extensions/BeforeChannelEntrySave.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace IllinoisPublicMedia\NprCds\Extensions;
+
+use ExpressionEngine\Service\Addon\Controllers\Extension\AbstractRoute;
+
+class BeforeChannelEntrySave extends AbstractRoute
+{
+    public function process($entry, $values)
+    {
+    }
+}
---
+++ ext.npr_cds.php
@@ -0,0 +1,12 @@
+<?php
+
+if (! defined('BASEPATH')) {
+    exit('No direct script access allowed');
+}
+
+use ExpressionEngine\Service\Addon\Extension;
+
+class Npr_cds_ext extends Extension
+{
+    protected $addon_name = 'npr_cds';
+}
