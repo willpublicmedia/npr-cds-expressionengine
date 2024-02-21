b4c7d358305d9115e0c2448f1c5e284b0877ad01
Andrew Reynolds <areynold@illinois.edu>
2024-02-21 16:25:11 -0600

    stub story creation

--- libraries/publishing/cds_parser.php
+++ libraries/publishing/cds_parser.php
@@ -18,7 +18,8 @@ class Cds_parser
         $json = $response->json;
 
         if (!empty($json->resources)) {
-            foreach ($json->resources as $story) {
+            foreach ($json->resources as $resource) {
+                $story = $this->create_story($resource);
                 $stories[] = $story;
             }
         }
@@ -28,6 +29,15 @@ class Cds_parser
         }
 
         dd($response);
+        throw new \Exception('not implemented');
+    }
+
+    private function create_story(object $json): object
+    {
+        // see: NPR_CDS_WP->update_posts_from_stories() line 165
+        // to do: check story exists && should be updated
+
+        dd($json);
         throw new \Exception('not implemented');
     }
 }
