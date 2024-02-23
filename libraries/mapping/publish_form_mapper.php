c8f271e183733f92b4a19105b9b0144597604652
Andrew Reynolds <areynold@illinois.edu>
2024-02-23 16:08:43 -0600

    add field migration notes

--- libraries/mapping/publish_form_mapper.php
+++ libraries/mapping/publish_form_mapper.php
@@ -13,6 +13,62 @@ class Publish_form_mapper
 {
     public function map($entry, $values, $story)
     {
+        /**
+         * @see https://npr.github.io/content-distribution-service/getting-started/story-api-migration-guide/table-of-fields.html
+         * [x] title
+         * [x] subtitle
+         * [x] teaser
+         * shortTitle -> socialTitle
+         * miniTeaser -> shortTeaser
+         * shortTeaser
+         * socialTitle
+         * contributorText -> shortTeaser
+         * thumbnail -> see image
+         * slug -> see slug
+         * id
+         * partnerId -> deprecated
+         * link type=api -> deprecated
+         * link type=html -> see webpages link
+         * storyDate -> publishedDate
+         * pubDate -> editorialLastModifiedDate
+         * publishedDate -> editorialLastModifiedDate
+         * lastModifiedDate -> deprecated
+         * audioRunByDate -> recommendUntilDateTime
+         * keywords -> deprecated
+         * priorityKeywords
+         * organization -> see organization
+         * parent ->  see parent
+         * byline -> see byline
+         * text
+         * textWithHtml -> text
+         * layout -> see layout
+         * relatedLink -> see relatedLink
+         * htmlAsset -> see htmlAsset
+         * multimedia -> see multimedia
+         * show -> see show
+         * correction -> see correction
+         * product -> deprecated
+         * promoArt -> deprecated
+         * staticGraphic -> deprecated
+         * performance -> deprecated
+         * fullStory -> deprecated
+         * fullText -> text
+         * listText -> deprecated
+         * message -> deprecated
+         * bookEdition deprecated
+         * book -> deprecated
+         * trait -> deprecated
+         * author -> deprecated
+         * externalAsset -> deprecated
+         * calendarEvent -> deprecated
+         * audio -> see audio
+         * pullQuote
+         * album -> see album
+         * artist -> see album
+         * transcript -> see transcript
+         * story -> see document
+         * image -> see image
+         */
         $url_title = $this->generate_url_title($entry, $story->title);
         $data = [
             'teaser' => $story->teaser,
