429c8c50e0fe7608c10fdf52fff464093876ead8
Andrew Reynolds <areynold@illinois.edu>
2024-09-23 16:59:43 -0500

    only update tags on pulled stories

--- Extensions/AfterChannelEntrySave.php
+++ Extensions/AfterChannelEntrySave.php
@@ -12,13 +12,50 @@ class AfterChannelEntrySave extends AbstractRoute
 {
     private $field_utils;
 
+    private $fields = array(
+        'channel_entry_source' => null,
+    );
+
+    public function __construct()
+    {
+        $this->map_model_fields(array_keys($this->fields));
+    }
+
     public function update_entry_tags($entry, $values)
     {
+        // check whether pulled story
+        $source_field = $this->fields['channel_entry_source'];
+        $is_external_story = array_key_exists($source_field, $values) ? $this->check_external_story_source($values[$source_field]) : false;
+
+        if (!$is_external_story) {
+            return;
+        }
+
         $tag_field = 'keywords';
         $tagger_installed = ee('Addon')->get('tagger')->isInstalled();
         if ($tagger_installed) {
             $this->run_tagger_hooks($entry, $tag_field);
         }
+    }
+
+    private function check_external_story_source($story_source)
+    {
+        if (is_null($story_source) || $story_source == 'local') {
+            return false;
+        }
+
+        return true;
+    }
+
+    private function map_model_fields($field_array)
+    {
+        $field_utils = new Field_utils();
+        $field_names = array();
+        foreach ($field_array as $model_field) {
+            $field_names[$model_field] = $field_utils->get_field_name($model_field);
+        }
+
+        $this->fields = $field_names;
     }
 
     private function run_tagger_hooks(ChannelEntry $entry, string $field_title): void
